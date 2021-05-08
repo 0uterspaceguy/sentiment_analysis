7f4b215bd784120c8c5d7aca2a91b48e43f09664
Krasnoyaruzhskiy <sergey.krasnoyaruzhskiy@gorparkovki.ru>
2021-05-08 11:28:00 +0300

    Add module with constant values

---
+++ CONST.py
@@ -0,0 +1,6 @@
+"""
+Module for containing some constant values
+"""
+
+punkts ='''!()-[]{};:'"\,<>./?@#$%^&*_~''' #punctuation list
+dictionary = set('абвгдеёжзийклмнопрстуфхцчшщъыьэюя1234567890.,!? ') #dicrionary for filter sentences
